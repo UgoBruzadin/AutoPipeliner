68741309f7ec9869d85613e5c6795450e4221c51
Ugo Bruzadin Nunes <ugob@siu.edu>
2023-01-25 10:57:54 -0600

    Update pipe_icrej.m

--- pipe_icrej.m
+++ pipe_icrej.m
@@ -19,14 +19,14 @@ end
 % -- flags the components
 EEG = pop_par_icflag(EEG, [NaN NaN; rej 1;rej 1;rej 1;rej 1;rej 1;NaN NaN]);
 
-printRejComponents(EEG)
+%print_RejComponents(EEG)
 %                    'Brain''Muscle''Eye''Heart''Line Noise''Channel Noise''Other' };
 %EEG = pop_par_icflag(EEG, [NaN NaN;flag 1;flag 1;flag 1;flag 1;flag 1;NaN NaN]);
 mybadcomps = find(EEG.reject.gcompreject);   %stores the Id of the components to be rejected
 
 if mybadcomps
 % --- prints rejected components at pwd
-printRejComponents(EEG)
+print_RejComponents(EEG)
 
 % --- rejects the components
 EEG = pop_par_subcomp(EEG, mybadcomps, 0);       % actually removes the flagged components
