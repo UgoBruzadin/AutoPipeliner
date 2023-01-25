c86f612843ff697bdf1ac7a22e492df3e1916b73
Ugo Bruzadin Nunes <ugob@siu.edu>
2023-01-25 10:57:49 -0600

    Update pipe_bss.m

--- pipe_bss.m
+++ pipe_bss.m
@@ -1,9 +1,12 @@
 
 function [EEG, acronym] = pipe_bss(EEG)
     
-window = EEG.trials*EEG.pnts/EEG.srate;
+%window = (EEG.pnts/EEG.srate)*2;
+window = (EEG.pnts/EEG.srate)*EEG.trials;
 
-[EEG] = pop_autobssemg( EEG, [window], [window], 'bsscca', {'eigratio', [1000000]}, 'emg_psd', {'ratio', [10],'fs', [250],'femg', [15],'estimator',spectrum.welch,'range', [0  49]});
+EEG = pop_autobssemg( EEG, [window], [window], 'bsscca', {'eigratio', [1000000]}, 'emg_psd', {'ratio', [10],'fs', EEG.srate,'femg', [15],'estimator',spectrum.welch,'range', [0  floor(EEG.nbchan/2)]});
+
+[EEGOUT,com] = pop_autobssemg( EEGIN, [window], [windowshift], 'bsscca', {'eigratio', [1000000]}, 'emg_psd', {'ratio', [10],'fs', EEGIN.srate,'femg', [15],'estimator',spectrum.welch,'range', [0  floor(EEGIN.nbchan/2)]});
 
 acronym = 'BSS';
 
